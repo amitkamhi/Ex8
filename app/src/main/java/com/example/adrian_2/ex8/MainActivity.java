f1a878d85072569c0bfe674747d29477c24062df
amitkamhi <amit.kamhi@gmail.com>
2016-12-06 17:01:03 +0200

    yolo

--- app/src/main/java/com/example/adrian_2/ex8/MainActivity.java
+++ app/src/main/java/com/example/adrian_2/ex8/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,12 @@ public class MainActivity extends AppCompatActivity implements TextWatcher{
         calculate.setOnClickListener(new calculateListener());
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
